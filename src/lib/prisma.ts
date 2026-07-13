fa5263c58726f9ba0bb93d2145a8f3d89c29f9a0
Gianluca <gianluca@example.com>
2026-07-13 11:20:27 -0300

    feat: initialize Prisma client with pg adapter singleton pattern

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,4 @@
-// @ts-ignore
-import { PrismaClient } from '.prisma/client/index.js'
+import { PrismaClient } from '@prisma/client'
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 
